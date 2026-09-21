46573b318af158fd7c228be112f9c51429ef87c0
Vinicius Sousa Palmeira <142844574+Vini09090@users.noreply.github.com>
2026-09-20 21:18:04 -0300

    Create aula1.cpp

---
+++ aula1.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    double inicio , fim , x;
+    if (x>inicio<fim){
+        cout << x << "Está dentro do intervalo \n";
+    }else {
+        cout<< "está fora do intervalo";
+    }
+    return 0;
+
+}
