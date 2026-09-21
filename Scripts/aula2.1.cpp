a09db7cf2fe0d1a4b15dd2e8dccd2d560653ede8
Vinicius Sousa Palmeira <142844574+Vini09090@users.noreply.github.com>
2026-09-20 21:21:39 -0300

    Create Aula2(2).cpp

---
+++ Scripts/aula2.1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+//área do triângulo 
+
+int main(){
+    int base, altura;
+    cout << "Digite a base e a altura : \n";
+    cin >> altura >> base;
+    
+    cout << "A área do Triangulo é : \n" << (altura + base) / 2 << "\n";
+    return 0;
+}
